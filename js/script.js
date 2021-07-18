269e4ddbc2551afadaf484c1f4b589b567b0e953
Albert-Byrone <albertbyrone1677@gmail.com>
2021-07-18 18:38:08 +0300

    create an object with the data

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const menu = [
+  {
+    id: 1,
+    title: "Butteril Cake",
+    category: "breakfast",
+    description:
+      "an online system for taking and processing orders in restaurants, with strong sales and marketing support. With direct integration with POS systems (such as Micros 3700 and others) and food ordering applications (such as Uber Eats, Glovo, Wolt, ",
+    image:
+      "https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+  },
+  {
+    id: 1,
+    title: "Butteril Cake",
+    category: "lunch",
+    description:
+      "an online system for taking and processing orders in restaurants, with strong sales and marketing support. With direct integration with POS systems (such as Micros 3700 and others) and food ordering applications (such as Uber Eats, Glovo, Wolt, ",
+    image:
+      "https://images.unsplash.com/photo-1508423134147-addf71308178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+  },
+  {
+    id: 1,
+    title: "Butteril Cake",
+    category: "dinner",
+    description:
+      "an online system for taking and processing orders in restaurants, with strong sales and marketing support. With direct integration with POS systems (such as Micros 3700 and others) and food ordering applications (such as Uber Eats, Glovo, Wolt, ",
+    image:
+      "https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+  },
+  {
+    id: 1,
+    title: "Butteril Cake",
+    category: "breakfast",
+    description:
+      "an online system for taking and processing orders in restaurants, with strong sales and marketing support. With direct integration with POS systems (such as Micros 3700 and others) and food ordering applications (such as Uber Eats, Glovo, Wolt, ",
+    image:
+      "https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+  },
+];
+
